216fb23d9c79161bb8e81e748166fc71596508ed
Prajapati jugesh <52920950+jugeshPrajapati@users.noreply.github.com>
2022-05-24 00:02:29 +0530

    hh

---
+++ Server.js
@@ -0,0 +1,23 @@
+//connection established
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
+//connection established
+
+//setting up internet routes
+app.use(require("./routes/record"));
